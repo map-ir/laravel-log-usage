ab49a142c14420310658e5e7756d63543e3002c4
armin nasiri <a.nasiri@map.ir>
2021-12-06 10:31:00 +0330

    add rdkafka

--- src/Http/Middleware/LogUsageMiddleware.php
+++ src/Http/Middleware/LogUsageMiddleware.php
@@ -7,11 +7,9 @@ namespace MapIr\LaravelLogUsage\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Kafka\Producer;
-use Kafka\ProducerConfig;
-use Monolog\Handler\SyslogHandler;
-use Monolog\Logger;
-
+use RdKafka\Conf;
+use RdKafka\Producer;
+use RdKafka\Exception;
 class LogUsageMiddleware
 {
     /**
@@ -45,39 +43,23 @@ class LogUsageMiddleware
         ], JSON_UNESCAPED_UNICODE);
 
         try {
-            $config = ProducerConfig::getInstance(); // create new config kafka
-            $config->setMetadataRefreshIntervalMs(config("logUsage.metadata_refresh_interval_ms"));
-            $config->setMetadataBrokerList(config('logUsage.kafka_server'));
-            $config->setBrokerVersion(config("logUsage.broker_version"));
-            $config->setRequiredAck(config("logUsage.required_ack"));
-            $config->setIsAsyn(config("logUsage.is_async"));
-            $config->setProduceInterval(500);
-            $producer =new Producer(
-                function() use($logData) {
-                    return [
-                        [
-                            'topic' => config('logUsage.topic'),
-                            'value' => $logData ,
-                            'key' => config('logUsage.key'),
-                        ],
-                    ];
-                }
-            );
-            $logger= new Logger('log_usage');
-            $logger->pushHandler(new SyslogHandler('log_usage'));
-            $producer->setLogger($logger);
-            $producer->success(function() {
+            $conf = new Conf();
+            $conf->set('metadata.broker.list', config('logUsage.kafka_server'));
+            $producer = new Producer($conf);
+            $topic = $producer->newTopic(config('logUsage.topic'));
+            $topic->produce(RD_KAFKA_PARTITION_UA, 0, $logData);
+            $producer->poll(0);
+            $result = $producer->flush(config("logUsage.metadata_refresh_interval_ms"));
+            if (RD_KAFKA_RESP_ERR_NO_ERROR !== $result) {
+                Log::error('Was unable to flush, messages might be lost!');
+
+            } else{
                 Log::info('Broker: successfully sent');
-            });
-            $producer->error(function() {
-                Log::info('Broker: failed to send');
-            });
-            $producer->send(true);
-        } catch (\Exception $exception) {
+            }
+        } catch (Exception $exception) {
             Log::error($exception->getMessage());
             return $response;
         }
-
         return $response;
     }
 }
